b4580319d8433146d05b1fcd49dcea8a0208e4b4
Karla Felicia Carvalho da Silva <60244518+feliciacarvalho@users.noreply.github.com>
2021-05-12 20:14:51 -0300

    Update raio.java

--- raio.java
+++ raio.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+/*Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais conforme exemplos.
+Fórmula da área: area = π . raio2
+Considere o valor de π = 3.14159  */
 
 public class raio
 {
